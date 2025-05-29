31d02b2713f1a066a117c2cd588fd27b43b47807
Rakurai3E7 <rakurai3e7@yahoo.com>
2025-05-29 02:26:59 -0500

    Update editor files

---
+++ editor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "editor.h"
+
+void init_editor() {
+    // blank
+    printf("Editor initialized.\n");
+}
+
+void move_cursor(char key) {
+    // blank
+    printf("Move cursor called with: %c\n", key);
+}
+
+void refresh_screen(void) {
+    // blank
+    printf("Screen refreshed.\n");
+}
+
+void cleanup_editor() {
+    // blank
+    printf("Editor cleaned up.\n");
+} 
---
+++ editor.h
@@ -0,0 +1,9 @@
+#ifndef EDITOR_H
+#define EDITOR_H
+
+void init_editor(void);
+void move_cursor(char key);
+void refresh_screen(void);
+void cleanup_editor(void);
+
+#endif
