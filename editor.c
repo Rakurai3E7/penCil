b523f46b653a1a8864b7258d62e2c33dae4f1a5f
Rakurai3E7 <rakurai3e7@yahoo.com>
2025-05-30 17:19:04 -0500

    Update editor.c, main.c

--- editor.c
+++ editor.c
@@ -16,5 +16,48 @@ void enableRawMode() {
     atexit(disableRawMode);
 
     struct termios raw = E.orig_termios;
-    
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+    raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
+    raw.c_cflag |= ~(CS8);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+}
+
+int getWindowSize(int *rows, int *cols) {
+    struct winsize ws;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1) return -1;
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+}
+
+void initEditor() {
+    E.cx = 0;
+    E.cy = 0;
+
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
+
+void editorRefreshScreen() {
+    write(STDOUT_FILENO, "\x1b[2J]", 4);
+    write(STDOUT_FILENO, "\x1b[H]", 3);
+
+    for (int y = 0; y < E.screenrows; y++) {
+        write(STDOUT_FILENO, "~\r\n", 3);
+    }
+}
+
+// super mininmal key input
+void processKeypress() {
+    char c;
+    if (read(STDIN_FILENO, &c, 1) == -1) return;
+
+    if (c == CTRL_KEY('q')) {
+        write(STDOUT_FILENO, "\x1b[2J]", 4); // clear screen
+        write(STDOUT_FILENO, "\x1b[H]", 3); // move home
+        exit(0);
+    }
 }
--- main.c
+++ main.c
@@ -1,22 +1,13 @@
-#include <stdio.h> 
-#include <stdlib.h>
-#include <unistd.h>
-#include <termios.h>
-#include <string.h>
-#include <ctype.h>
-#include <fcntl.h>
-#include <dirent.h>
-#include <limits.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <errno.h>
-#include <regex.h>
-#include <dlfcn.h>
-#include <signal.h>
-#include <time.h>
-#include <iconv.h>
+#include "editor.h"
 
 int main()
 {
+    enableRawMode();
+    initEditor();
+
+    while (1) {
+        editorRefreshScreen();
+        processKeypress();
+    }
     return 0;
 }
